96af8d4b08c5788f24766b4e24ff1c67c3c06da2
Donnie Ellstrom <dsvene@gmail.com>
2017-03-31 12:05:16 -0700

    Webpack config / setup index.js

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -7,12 +7,13 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div> Hellow World I am React Yeah</div>
+      <div> Hellow World I am React!</div>
     )
   }
 });
 
 ReactDOM.render(
+  // Invoking component
   <HelloWorld />,
   document.getElementById('app')
 );
