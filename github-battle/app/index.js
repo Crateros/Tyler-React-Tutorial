4af7de2b7dc0b3ef95a7ff90f6aa9ee0241f0f02
Donnie Ellstrom <dsvene@gmail.com>
2017-03-31 13:43:29 -0700

    Add avatar parent component, add profile name, link, and pic child components

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,19 +1,75 @@
 // var app = document.getElementById('app');
 // app.innerHTML = "Hello";
 
+// fn(d) = V --> function that takes in (data) and outputs a View
+
+//React components should be F.I.R.S.T.
+  // Focused
+  // Independent
+  // Reusable
+  // small
+  // Testable
+
+var USER_DATA = {
+  name: "Donnie Ellstrom Codes React",
+  username: "Crateros",
+  image: 'https://avatars1.githubusercontent.com/u/17658464?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
+// var HelloWorld = React.createClass({
+//   render: function() {
+//     console.log(this.props)
+//     return (
+//       <div> Hello World I am React! My creator is {this.props.name} and I am {this.props.anyDataHere} years old. </div>
+//     )
+//   }
+// });
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageURL} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
     return (
-      <div> Hellow World I am React!</div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageURL={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
   // Invoking component
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
