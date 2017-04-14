38c3eea58c9104f71c434fe2f72b65af3eb5b681
Donnie Ellstrom <dsvene@gmail.com>
2017-04-14 14:10:45 -0700

    Finalize popular component, establish github api util

--- github-battle-2.0/app/components/Popular.js
+++ github-battle-2.0/app/components/Popular.js
@@ -1,41 +1,139 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+//Stateless functional component, pass in props as argument, no state, receives everything as props
+function SelectLanguage(props) {
+
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="languages">
+      {languages.map(function(language) {
+        // console.log("This", this);
+        return (
+          <li
+            style={language === props.selectedLanguage ? { color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, language)}
+            key={language}>
+            {language}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+//Can turn this into a stateless function (above), make sure to pass in props as argument
+// class SelectLanguage extends React.Component {
+//   render() {
+//     return (
+//       <ul className="languages">
+//         {languages.map(function(language) {
+//           // console.log("This", this);
+//           return (
+//             <li
+//               style={language === this.props.selectedLanguage ? { color: '#d0021b'} : null}
+//               onClick={this.props.onSelect.bind(null, language)}
+//               key={language}>
+//               {language}
+//             </li>
+//           )
+//         })}
+//       </ul>
+//     )
+//   }
+// }
+
+function RepoGrid(props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function(repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for' + repo.owner.login} />
+              </li>
+              <li>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+              <li>
+                @{repo.owner.login}
+              </li>
+              <li>
+                {repo.stargazers_count} stars
+              </li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  //check for componentDidMount before executing API call
+  componentDidMount() {
+    //AJAX in here. updateLanguage function invokes API call function
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+
   updateLanguage(language) {
     this.setState(function () {
         return {
-          selectedLanguage: language
+          selectedLanguage: language,
+          repos: null
         }
     });
+    //Fetch repo info on updateLanguage click
+    api.fetchPopularRepos(language)
+      .then(function(repos) {
+        console.log(repos);
+        this.setState(function() {
+        return {
+          repos: repos,
+        }
+        })
+      }.bind(this));
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
     return (
-      <ul className="languages">
-        {/* <p>Selected: {this.state.selectedLanguage}</p> */}
-        {languages.map(function(language) {
-          // console.log("This", this);
-          return (
-            <li
-              style={language === this.state.selectedLanguage ? { color: '#d0021b'} : null}
-              onClick={this.updateLanguage.bind(null, language)}
-              key={language}>
-              {language}
-            </li>
-          )
-        }, this)}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}/>
+          {/* {JSON.stringify(this.state.repos, null, 2)} */}
+          {/* Wait for repos to get returned from api before attempting to render RepoGrid */}
+          {!this.state.repos
+            ? <p>LOADING...</p>
+            : <RepoGrid repos={this.state.repos} />}
+      </div>
     )
   }
 }
---
+++ github-battle-2.0/app/utils/api.js
@@ -0,0 +1,13 @@
+var axios = require('axios');
+
+//Exports these methods as object items for use in other components
+module.exports = {
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+    });
+  }
+}
