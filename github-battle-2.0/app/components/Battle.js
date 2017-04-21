befdecf24c4901cabecfa3609be15de0019e6da2
Donnie Ellstrom <dsvene@gmail.com>
2017-04-20 17:20:10 -0700

    Add results component, adjust routes

--- github-battle-2.0/app/components/Battle.js
+++ github-battle-2.0/app/components/Battle.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
+//Stateless functional component
 function PlayerPreview(props) {
   console.log("Player Preview props: ", props);
   return (
@@ -14,6 +16,7 @@ function PlayerPreview(props) {
       </div>
       <button
         className='reset'
+        // Needs the id of which player is requesting reset
         onClick={props.onReset.bind(null, props.id)}>
           Reset
       </button>
@@ -39,6 +42,7 @@ class PlayerInput extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  //Updates form input field when user enters username
   handleChange(event) {
     var value = event.target.value;
 
@@ -107,7 +111,7 @@ class Battle extends React.Component {
   }
 
   handleSubmit(id, username) {
-    this.setState(function () {
+    this.setState(function() {
       var newState = {};
       newState[id + 'Name'] = username;
       newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
@@ -125,16 +129,17 @@ class Battle extends React.Component {
   }
 
   render() {
+    var match = this.props.match;
     var playerOneName = this.state.playerOneName;
     var playerOneImage = this.state.playerOneImage;
     var playerTwoName = this.state.playerTwoName;
     var playerTwoImage = this.state.playerTwoImage;
 
-    {/* If then syntax, if playerOneName falsey render PlayerInput */}
 
     return (
       <div>
         <div className='row'>
+          {/* If then syntax, if playerOneName falsey render PlayerInput */}
           {!playerOneName &&
             <PlayerInput
               id='playerOne'
@@ -165,6 +170,20 @@ class Battle extends React.Component {
           id='playerTwo'/>
           }
         </div>
+
+        {/* If playerOneImage & playerTwoImage truthy then render Link */}
+        {playerOneImage && playerTwoImage &&
+          // This link takes player to current url (/battle) using the match.url method, then appends '/results' to /battle url and passes along query
+          <Link
+            className='button'
+            to={{
+              pathname: match.url + '/results',
+              search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+            }} >
+            Battle
+          </Link>
+        }
+
       </div>
     )
   }
